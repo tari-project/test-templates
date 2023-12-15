612a5e9666e1c8e7d6031b42fc21112db9948c75
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2023-12-15 15:54:50 +0000

    test setup

--- src/nft_marketplace/tests/test.rs
+++ src/nft_marketplace/tests/test.rs
@@ -1,21 +1,37 @@
 use tari_template_lib::args;
 use tari_template_lib::models::{
-    Amount, Bucket, ComponentAddress, NonFungibleAddress, ResourceAddress,
+    ComponentAddress, NonFungibleAddress, ResourceAddress,
 };
 use tari_template_test_tooling::crypto::RistrettoSecretKey;
 use tari_template_test_tooling::TemplateTest;
 use tari_transaction::Transaction;
+use tari_template_lib::prelude::Metadata;
+use tari_template_test_tooling::SubstateType;
 
 #[test]
-fn it_compiles() {
+fn auction_period_ends_with_winning_bid() {
     let TestSetup {
         mut test,
         marketplace_component,
-        admin_account,
-        admin_proof,
-        admin_key,
+        seller_account,
         ..
     } = setup();
+
+    // create the NFT that is going to be sold
+
+
+    // create an auction for the NFT
+
+    // place a valid bid
+
+    // place a higher bid (the previous bid will be refunded to the bidder)
+
+    // advance the epoch so the auction period expires
+
+    // the winning bidder withdraws the NFT
+
+    // the seller received the bid payment
+
 
     /*
     let result = test.execute_expect_success(
@@ -37,48 +53,84 @@ fn it_compiles() {
     */
 }
 
+// TODO: auction_period_ends_with_no_winning_bid
+// TODO: auction_finishes_by_buying_price_bid
+// TODO: auction_cancelled_by_seller
+// TODO: it_rejects_invalid_auctions
+// TODO: it_rejects_invalid_bids
+// TODO: it_rejects_invalid_auction_withdrawals
+// TODO: it_rejects_invalid_auction_cancellations
+
 struct TestSetup {
     test: TemplateTest,
     marketplace_component: ComponentAddress,
-    admin_account: ComponentAddress,
-    admin_proof: NonFungibleAddress,
-    admin_key: RistrettoSecretKey,
+    seller_account: ComponentAddress,
+    seller_owner_token: NonFungibleAddress,
+    seller_key: RistrettoSecretKey,
     seller_badge_resource: ResourceAddress,
+    seller_nft_address: NonFungibleAddress,
 }
 
 fn setup() -> TestSetup {
     let mut test = TemplateTest::new(["./"]);
-    let (admin_account, admin_proof, admin_key) = test.create_owned_account();
-    let template = test.get_template_address("NftMarketplace");
 
+    // create the seller account
+    let (seller_account, seller_owner_token, seller_key) = test.create_owned_account();
+    
+    // create the NFT marketplace component
+    let template = test.get_template_address("NftMarketplace");
     let result = test.execute_expect_success(
         Transaction::builder()
             .call_function(template, "new", args![])
-            .sign(&admin_key)
+            .sign(&seller_key)
             .build(),
-        vec![admin_proof.clone()],
+        vec![seller_owner_token.clone()],
     );
-
     let marketplace_component = result.finalize.execution_results[0]
         .decode::<ComponentAddress>()
         .unwrap();
-
     let indexed = test
         .read_only_state_store()
         .inspect_component(marketplace_component)
         .unwrap();
-
     let seller_badge_resource = indexed
         .get_value("$.seller_badge_resource")
         .unwrap()
         .expect("seller_badge_resource not found");
 
+    // create a new account NFT that the seller is going to put on sale
+    let account_nft_template = test.get_template_address("AccountNonFungible");
+    let result = test.execute_expect_success(
+        Transaction::builder()
+            .call_function(account_nft_template, "create", args![seller_owner_token])
+            .sign(&seller_key)
+            .build(),
+        vec![seller_owner_token.clone()],
+    );
+    let account_nft_component = result.finalize.execution_results[0].decode::<ComponentAddress>().unwrap();
+
+    let mut nft_metadata = Metadata::new();
+    nft_metadata.insert("name".to_string(), "my_custom_nft".to_string());
+
+    let result = test.execute_expect_success(
+        Transaction::builder()
+            .call_method(account_nft_component, "mint", args![nft_metadata])
+            .put_last_instruction_output_on_workspace("nft_bucket")
+            .call_method(seller_account, "deposit", args![Workspace("nft_bucket")])
+            .sign(&seller_key)
+            .build(),
+        vec![seller_owner_token.clone()],
+    );
+    let output = test.get_previous_output_address(SubstateType::NonFungible);
+    let seller_nft_address = output.as_non_fungible_address().unwrap().clone();
+
     TestSetup {
         test,
         marketplace_component,
-        admin_account,
-        admin_proof,
-        admin_key,
+        seller_account,
+        seller_owner_token,
+        seller_key,
         seller_badge_resource,
+        seller_nft_address,
     }
 }
