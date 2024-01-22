860da44fe867d6fcd1aa7e118b20dcdb104823f2
Stan Bondi <sdbondi@users.noreply.github.com>
2024-01-22 14:53:06 +0400

    chore: update dists for faucet and tariswap

--- src/tariswap/src/lib.rs
+++ src/tariswap/src/lib.rs
@@ -20,7 +20,7 @@
 //   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 //   USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use tari_template_abi::rust::collections::HashMap;
+use std::collections::HashMap;
 use tari_template_lib::prelude::*;
 
 #[template]
